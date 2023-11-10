35e0017c54993bb527eb87af400d50ac55360b81
ivanlev <fanatos@mail.ru>
2023-11-10 19:52:55 +0500

    style: убрать лишний пробел

--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PopupWithForm({ isOpen, onClose, onSubmit, title, name, submitButtonText, ...props }) {
   return (
-    <div className={`popup popup_type_${name}  ${isOpen ? 'popup_opened' : ''}`}>
+    <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__container">
         <p className="popup__heading">{title}</p>
         <form
