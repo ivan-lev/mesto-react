28b3316554559b15e4968e80f935311e420bf481
ivanlev <fanatos@mail.ru>
2023-11-10 20:02:05 +0500

    style: удалить комментарии

--- src/components/DeleteCardPopup.jsx
+++ src/components/DeleteCardPopup.jsx
@@ -16,28 +16,6 @@ function DeleteCardPopup({ isOpen, onClose, onCardDelete }) {
       name="delete-card"
       submitButtonText="Удалить"
     />
-    // <div className={`popup popup_type_delete-card  ${isOpen ? 'popup_opened' : ''}`}>
-    //   <div className="popup__container">
-    //     <p className="popup__heading">Вы уверены?</p>
-    //     <form
-    //       id="popup__user-form"
-    //       name="popup_type_delete-card"
-    //       className="popup__form"
-    //       onSubmit={handleDeleteCard}
-    //     >
-    //       <button type="submit" className="button popup__save-user-data popup__submit-button">
-    //         Удалить
-    //       </button>
-
-    //       <button
-    //         id="close-edit-user-button"
-    //         type="button"
-    //         className="button popup__close-button"
-    //         onClick={onClose}
-    //       ></button>
-    //     </form>
-    //   </div>
-    // </div>
   );
 }
 
