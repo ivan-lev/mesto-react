2e40b024cf0d38fd203be32600c3f209edc0e611
ivanlev <fanatos@mail.ru>
2023-11-10 20:00:59 +0500

    feat: переиспользовать компоненты

--- src/components/DeleteCardPopup.jsx
+++ src/components/DeleteCardPopup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PopupWithForm from './PopupWithForm';
 
 function DeleteCardPopup({ isOpen, onClose, onCardDelete }) {
   function handleDeleteCard(event) {
@@ -7,28 +8,36 @@ function DeleteCardPopup({ isOpen, onClose, onCardDelete }) {
   }
 
   return (
-    <div className={`popup popup_type_delete-card  ${isOpen ? 'popup_opened' : ''}`}>
-      <div className="popup__container">
-        <p className="popup__heading">Вы уверены?</p>
-        <form
-          id="popup__user-form"
-          name="popup_type_delete-card"
-          className="popup__form"
-          onSubmit={handleDeleteCard}
-        >
-          <button type="submit" className="button popup__save-user-data popup__submit-button">
-            Удалить
-          </button>
+    <PopupWithForm
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleDeleteCard}
+      title="Вы уверены?"
+      name="delete-card"
+      submitButtonText="Удалить"
+    />
+    // <div className={`popup popup_type_delete-card  ${isOpen ? 'popup_opened' : ''}`}>
+    //   <div className="popup__container">
+    //     <p className="popup__heading">Вы уверены?</p>
+    //     <form
+    //       id="popup__user-form"
+    //       name="popup_type_delete-card"
+    //       className="popup__form"
+    //       onSubmit={handleDeleteCard}
+    //     >
+    //       <button type="submit" className="button popup__save-user-data popup__submit-button">
+    //         Удалить
+    //       </button>
 
-          <button
-            id="close-edit-user-button"
-            type="button"
-            className="button popup__close-button"
-            onClick={onClose}
-          ></button>
-        </form>
-      </div>
-    </div>
+    //       <button
+    //         id="close-edit-user-button"
+    //         type="button"
+    //         className="button popup__close-button"
+    //         onClick={onClose}
+    //       ></button>
+    //     </form>
+    //   </div>
+    // </div>
   );
 }
 
