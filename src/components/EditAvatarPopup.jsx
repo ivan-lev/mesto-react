2e5c346d8edd3dc464e097c70ce246a26b1ca998
ivanlev <fanatos@mail.ru>
2023-11-10 19:45:19 +0500

    feat: переиспользовать компоненты

--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -1,4 +1,5 @@
 import { React, useRef, useEffect } from 'react';
+import PopupWithForm from './PopupWithForm.jsx';
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
   const avatarRef = useRef();
@@ -15,40 +16,27 @@ function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
   };
 
   return (
-    <div className={`popup popup_type_edit-avatar ${isOpen ? 'popup_opened' : ''}`}>
-      <div className="popup__container">
-        <p className="popup__heading">Обновить аватар</p>
-        <form
-          id="popup__user-form"
-          name={`popup_type_edit-avatar`}
-          className="popup__form"
-          onSubmit={handleSubmit}
-        >
-          <fieldset className="popup__fieldset">
-            <input
-              type="url"
-              placeholder="Ссылка на аватарку"
-              id="avatar-photo-link"
-              name="avatar-photo-link"
-              className="popup__input popup__input_type_avatar-photo-link"
-              required
-              ref={avatarRef}
-            />
-            <span className="popup__error avatar-photo-link-error"></span>
-          </fieldset>
-          <button type="submit" className="button popup__save-user-data popup__submit-button">
-            Сохранить
-          </button>
-
-          <button
-            id="close-edit-user-button"
-            type="button"
-            className="button popup__close-button"
-            onClick={onClose}
-          ></button>
-        </form>
-      </div>
-    </div>
+    <PopupWithForm
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      title="Обновить аватар"
+      name="profile"
+      submitButtonText="Сохранить"
+    >
+      <fieldset className="popup__fieldset">
+        <input
+          type="url"
+          placeholder="Ссылка на аватарку"
+          id="avatar-photo-link"
+          name="avatar-photo-link"
+          className="popup__input popup__input_type_avatar-photo-link"
+          required
+          ref={avatarRef}
+        />
+        <span className="popup__error avatar-photo-link-error"></span>
+      </fieldset>
+    </PopupWithForm>
   );
 }
 
